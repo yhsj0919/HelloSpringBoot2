309a14e52a0acfea8c63abb907f4831bd37b17da
LOVE <1130402124@qq.com>
2018-04-25 00:36:05 +0800

    初次上传

---
+++ src/main/kotlin/xyz/yhsj/spring/loginfo/LoggerConfguration.java
@@ -0,0 +1,23 @@
+package xyz.yhsj.spring.loginfo;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * ========================
+ * Created with IntelliJ IDEA.
+ * User：恒宇少年
+ * Date：2017/4/10
+ * Time：21:52
+ * 码云：http://git.oschina.net/jnyqy
+ * ========================
+ */
+@Configuration
+public class LoggerConfguration implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor()).addPathPatterns("/**");
+    }
+}
